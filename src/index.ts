0d3c70f8a8c25ca70303a2be81f70eac1b03970b
enzoesper <enzoesper@hotmail.com>
2021-11-15 17:45:37 -0300

    se agrega export navbar

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import StyledButton from "./components/StyledButton/styled-button";
 import theme from "./themes/theme";
 import NotFound from "./components/NotFound";
 import StepperHorizontal from "./components/StepperHorizontal";
+import NavBar from "./components/NavBar";
 
-export { StyledButton, theme, NotFound, StepperHorizontal };
+export { StyledButton, theme, NotFound, StepperHorizontal, NavBar };
