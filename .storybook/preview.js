82fd07ef5b9da29213307821b467f9f2ab923547
enzoesper <enzoesper@hotmail.com>
2021-11-23 02:37:46 -0300

    probando solucion auth0provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core";
 import { StylesProvider } from "@material-ui/core";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import theme from "../src/themes/theme";
 
 // Adds a global decorator to include the imported styles from the design system.
 export const decorators = [
   (Story) => (
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <Story />
-      </ThemeProvider>
-    </StylesProvider>
+    <Auth0Provider>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Story />
+        </ThemeProvider>
+      </StylesProvider>
+    </Auth0Provider>
   ),
 ];
 
