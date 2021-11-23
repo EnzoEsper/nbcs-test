02a85295bccbe20a6ad34454b497c0ff2fa1ccb1
enzoesper <enzoesper@hotmail.com>
2021-11-22 22:05:32 -0300

    se reinstalan paquetes para solucionar error: does not contain a default export (imported as 'require$$0').

--- src/components/AvatarPerfil/AvatarPerfil.tsx
+++ src/components/AvatarPerfil/AvatarPerfil.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { Theme } from "@material-ui/core/styles";
 import Avatar from "@material-ui/core/Avatar";
-import { withStyles } from "@material-ui/styles";
 import { AvatarPerfilProps } from "./AvatarPerfil.types";
-import { AvatarPerfilState } from "./AvatarPerfilState";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import { useStyles } from "./AvatarPerfil.styles";
 
